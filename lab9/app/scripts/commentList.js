e7b0579dd186a79d19df6f5434efbc46f6173c81
kjhark24 <kjhark24@gmail.com>
2016-11-04 18:07:01 -0400

    lab 9 done
    yupp

---
+++ lab9/app/scripts/commentList.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Remarkable from 'remarkable';
+
+import Comment from './comment.js';
+
+module.exports = React.createClass({
+  render: function() {
+    var commentNodes = this.props.data.map(function(comment) {
+      return (
+        <Comment author={comment.author} key={comment.id}>
+          {comment.text}
+        </Comment>
+      );
+    });
+    return (
+      <div className="commentList">
+        {commentNodes}
+      </div>
+    );
+  }
+});
